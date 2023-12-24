9ee55acbe1c1671de5a51aa1be910639c68f9388
Stuart <chanstuart@yahoo.com>
2023-12-23 23:13:19 -0500

    analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import Clock from './Clock'
 import QuotesCarousel from './QuotesCarousel'
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -11,5 +12,6 @@ root.render(
     <App />
     <Clock />
     <QuotesCarousel interval={10000} />
+    <Analytics />
   </React.StrictMode>
 )
