ecf8e0f40a30ac4ed7998413a9a1cac2d2d56181
Stuart <chanstuart@yahoo.com>
2023-12-24 00:18:33 -0500

    speed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import Clock from './Clock'
 import QuotesCarousel from './QuotesCarousel'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -13,5 +14,6 @@ root.render(
     <Clock />
     <QuotesCarousel interval={10000} />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 )
