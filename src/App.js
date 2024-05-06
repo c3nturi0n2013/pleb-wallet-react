d6e53e1d638c5d09e4e865c5134f9382216c939b
Stuart <chanstuart@yahoo.com>
2024-05-06 13:15:30 -0400

    advancing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [price, setPrice] = useState(0)
+  const [price, setPrice] = useState(0);
+  const [bitcoinData, setBitcoinData] = useState({ amount: '', percent: '', date: '' });
 
   const getPrice = () => {
     axios
       .get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
       .then((res) => {
-        setPrice(Number(res.data.data.amount))
+        setPrice(Number(res.data.data.amount));
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPrice();
+    const interval = setInterval(getPrice, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get('https://raw.githubusercontent.com/c3nturi0n2013/data/master/btc.json')
+      .then((res) => {
+        const data = res.data[0]; // Assuming data you want is at the first index
+        setBitcoinData({
+          amount: data.amount,
+          percent: data.percent,
+          date: data.date,
+        });
       })
-  }
-
-  useEffect(() => {
-    getPrice()
-  }, [])
-
-  useEffect(() => {
-    // setInterval will run whatever is in the callback function every 5 seconds
-    const interval = setInterval(() => {
-      getPrice()
-    }, 5000)
-    return () => clearInterval(interval)
-  }, [])
+      .catch((err) => {
+        console.log('Error fetching Bitcoin data:', err);
+      });
+  }, []);
 
   return (
     <div className="container">
@@ -37,9 +48,11 @@ function App() {
       <h4>
         One dollar can buy you {(1 / (price / 100000000)).toFixed(2)} sats
       </h4>
-      <h4>12.166% BTC owned by publicly traded & private companies (May 4, 2024)</h4>
+      <h4 id='gizmo'>
+        {bitcoinData.percent}% BTC owned as of {bitcoinData.date} with amount ${bitcoinData.amount}
+      </h4>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
