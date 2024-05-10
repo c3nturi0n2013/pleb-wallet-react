88f60d78b4153d4920987a8155b73e41a49a35ed
Stuart <chanstuart@yahoo.com>
2024-05-10 08:38:25 -0400

    import strike img

--- src/ReferralCard.js
+++ src/ReferralCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './referral.css'
+import strikeImage from './strike.png'; 
 
 const ReferralCard = () => {
     return (
         <div className="referral-card" onClick={() => window.open('https://invite.strike.me/7POMSO', '_blank')}>
-          <img src="./strike.png" alt="Strike Logo" className="card-image"/>
+          <img src={strikeImage} alt="Strike Logo" className="card-image"/>
             <div className="card-content">
                 <h2>Join Strike Today!</h2>
                 <p>Use this referral link to join Strike and start using Bitcoin easily!</p>
