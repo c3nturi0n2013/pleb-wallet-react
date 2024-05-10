f5691d72975177575c24f959d60b5fd86d227d8b
Stuart <chanstuart@yahoo.com>
2024-05-10 11:26:15 -0400

    may 10

--- src/BtcEtf.js
+++ src/BtcEtf.js
@@ -46,7 +46,7 @@ function BtcEtf() {
                     ))}
                 </tbody>
             </table>
-            <span>**Updated May 9th 2024 by Yahoo</span>
+            <span>**Updated May 10th 2024 by Yahoo</span>
         </div>
     );
 }
